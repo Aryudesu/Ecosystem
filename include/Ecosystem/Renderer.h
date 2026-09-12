cd801f2b800b8b8ff8a399225d98791712ec5bbb
Aryu <suwako_a-u-@hotmail.co.jp>
2026-09-12 19:31:43 +0900

    Add sprite renderer interface

---
+++ include/Ecosystem/Renderer.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace ecosystem {
+class Simulation;
+}
+
+namespace ecosystem::graphics {
+
+void DrawSimulation(const Simulation& simulation);
+
+} // namespace ecosystem::graphics

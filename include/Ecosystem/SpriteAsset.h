11e864cc9e702bca5a31f4c0a472abef4c4b374a
Aryu <suwako_a-u-@hotmail.co.jp>
2026-09-13 15:11:29 +0900

    種スプライトと左右反転描画を追加

--- include/Ecosystem/SpriteAsset.h
+++ include/Ecosystem/SpriteAsset.h
@@ -8,11 +8,12 @@ enum class SpriteIndex : std::size_t {
     Grass = 0,
     Herbivore = 1,
     Carnivore = 2,
+    Seed = 3,
 };
 
 bool InitializeSprites();
 void ShutdownSprites();
-bool DrawSprite(SpriteIndex sprite, int centerX, int centerY);
+bool DrawSprite(SpriteIndex sprite, int centerX, int centerY, bool flipHorizontal = false);
 bool IsSpriteReady();
 
 } // namespace ecosystem::graphics

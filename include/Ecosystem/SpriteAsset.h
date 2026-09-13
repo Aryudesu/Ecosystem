c1d29aa4638be044dbfb8b4e878232baa5ec3457
Aryu <suwako_a-u-@hotmail.co.jp>
2026-09-13 15:58:52 +0900

    Organize sprite sheet indices by row

--- include/Ecosystem/SpriteAsset.h
+++ include/Ecosystem/SpriteAsset.h
@@ -6,15 +6,17 @@ namespace ecosystem::graphics {
 
 enum class SpriteIndex : std::size_t {
     Grass = 0,
-    HerbivoreWalk1 = 1,
-    HerbivoreWalk2 = 2,
-    HerbivoreRun1 = 3,
-    HerbivoreRun2 = 4,
-    CarnivoreWalk1 = 5,
-    CarnivoreWalk2 = 6,
-    CarnivoreRun1 = 7,
-    CarnivoreRun2 = 8,
-    Seed = 9,
+    Seed = 1,
+
+    HerbivoreWalk1 = 4,
+    HerbivoreWalk2 = 5,
+    HerbivoreRun1 = 6,
+    HerbivoreRun2 = 7,
+
+    CarnivoreWalk1 = 8,
+    CarnivoreWalk2 = 9,
+    CarnivoreRun1 = 10,
+    CarnivoreRun2 = 11,
 };
 
 bool InitializeSprites();
